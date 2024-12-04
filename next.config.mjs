762fff74f6fbde061a0e4340014d98a86e92a9c3
mohamedkahmed <mksalem614@gmail.com>
2024-12-04 23:01:46 +0200

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'figma.com',
-            }
-        ]
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
