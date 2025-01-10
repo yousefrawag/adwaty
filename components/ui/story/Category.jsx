cc0eb3a8576cd46cf5969bd142097c371b009962
mohamedkahmed <mksalem614@gmail.com>
2025-01-10 14:13:08 +0200

    v20

--- components/ui/story/Category.jsx
+++ components/ui/story/Category.jsx
@@ -7,6 +7,7 @@ import Link from "next/link.js";
 import { CiCirclePlus } from "react-icons/ci";
 import { AuthFetchBlog } from "@/utils/FetchSeries";
 import ModulePop from "@/components/common/modulepop";
+import { FaPlus } from "react-icons/fa";
 const Category = () => {
   const { loading, data } = AuthFetchBlog();
   const categories = data.categories || [];
@@ -38,7 +39,7 @@ const Category = () => {
   return (
     <div>
       {/* parent */}
-      <div className="py-10 ">
+      <div className="py-10">
         <div className="flex gap-4 items-center">
           {unique_Data?.map((category) => {
             return (
@@ -57,12 +58,12 @@ const Category = () => {
           })}
         </div>
         {myData.length ? (
-          <div className="grid grid-cols-1 xl:grid-cols-3 md:grid-cols-2 lg:grid-cols-3 gap-4 pt-10">
+          <div className="grid grid-cols-1 xl:grid-cols-4 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-10">
             {myData?.map((item) => {
               return (
                 <div
                   key={item.id}
-                  className="group shadow-lg w-[100%] relative h-[300px] bg-red-300 rounded overflow-hidden "
+                  className="group transition ease-in-out duration-300 hover:scale-110 shadow-lg w-[100%] relative h-[300px] bg-red-300 rounded overflow-hidden"
                 >
                   <div>
                     <Image
@@ -73,12 +74,12 @@ const Category = () => {
                       objectFit="cover"
                     />
                   </div>
-                  <div className="absolute flex flex-col items-start px-6 justify-center transition-all duration-200 group-hover:translate-y-0 w-full h-[50%] bg-[#CDCBC9] bottom-0 translate-y-full lef-0">
-                    <h1 className="text-[#3C3F46] font-bold text-[16px]">
+                  <div className="absolute flex flex-col items-start px-6 pb-6 justify-end transition-all duration-200 group-hover:translate-y-0 w-full h-[100%] bottom-0 translate-y-full lef-0" style={{background: "linear-gradient(0deg,#0d1c27 11.97%,transparent)"}}>
+                    <h1 className="text-[#ffffff] font-bold text-[16px]">
                       {item.title}
                     </h1>
                     <p
-                      className="text-[#3C3F46] text-[14px]"
+                      className="text-[#dddddd] text-[14px]"
                       style={{
                         overflow: "hidden",
                         display: "-webkit-box",
@@ -91,7 +92,7 @@ const Category = () => {
                     <div className="bg-[#2D3036] items-center mt-2 text-white flex gap-2 py-2 rounded-full px-2">
                       <button
                         onClick={() => handelpopup(item?.promoLink)}
-                        className="flex items-center gap-2"
+                        className="flex items-center gap-2 hover:opacity-50 duration-200"
                       >
                         <span className="w-6 h-6 flex items-center justify-center rounded-full bg-gradient-to-r from-[#00CC9A] to-[#009BFB]">
                           <FaPlay className="text-[10px]" />
@@ -100,11 +101,11 @@ const Category = () => {
                       </button>
                       <Link
                         href={`/series/${item.id}`}
-                        className="flex items-center gap-2"
+                        className="flex items-center gap-2 hover:opacity-50 duration-200"
                       >
-                        <span>
-                          <CiCirclePlus className="text-[18px]" />{" "}
-                        </span>{" "}
+                        <span className="w-6 h-6 flex items-center justify-center rounded-full bg-gradient-to-r from-[#00CC9A] to-[#009BFB]">
+                          <FaPlus className="text-[12px] mr-[1px]" />
+                        </span>
                         <span className="text-[12px]">المزيد</span>
                       </Link>
                     </div>
--- components/ui/story/Story.jsx
+++ components/ui/story/Story.jsx
@@ -3,7 +3,7 @@ import Category from "./Category"
 
 const Story = () => {
   return (
-    <section className="dark:bg-dark dark:text-white bg-white py-pp_80 px-5">
+    <section className="dark:bg-dark overflow-hidden dark:text-white bg-white py-pp_80 px-5">
       <div className="container mx-auto">
       <div>
         <h1>القصص الرائعة تبدأ هنا</h1>
