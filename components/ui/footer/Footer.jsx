1cf1b3c632b886ec37a8764f8042f772472daee6
yousefrawag <yousefrawag908@gmail.com>
2025-01-14 10:14:28 +0200

    new-fix dark-mode

--- components/ui/footer/Footer.jsx
+++ components/ui/footer/Footer.jsx
@@ -7,7 +7,7 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <footer className='py-pp_80 dark:bg-dark dark:text-white'>
+    <footer className='py-pp_80  text-black'>
       <div className="container mx-auto">
       <div className='grid grid-cols-1   xl:grid-cols-3 lg:grid-cols-3 gap-8'>
         <div className='w-full px-10 xl:px-0 lg:px-0'>
